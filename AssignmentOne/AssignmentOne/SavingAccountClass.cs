47723fdc45567a92731476506461785445b977d9
usmanAfzalKhan <109322442+usmanAfzalKhan@users.noreply.github.com>
2022-07-15 00:33:30 -0400

    Add files via upload

---
+++ AssignmentOne/AssignmentOne/SavingAccountClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    class SavingAccountClass: AccountClass
+    {
+        public SavingAccountClass(double _balance, int initAccountNumber, string initAccountHolderName, double initBalance, double initAnnualInterestRate) 
+            : base(_balance, initAccountNumber, initAccountHolderName, initBalance, initAnnualInterestRate)
+        {
+            AnnualInterestRate = 0.03;
+            double savings = Balance - _balance;
+
+
+        }
+    }
+}
---
+++ AssignmentOne/AssignmentOne/TransactionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    class TransactionClass
+    {
+        enum TransactionType
+        {
+            WITHDRAW, DEPOSIT
+        }
+        private readonly double Amount;
+        private readonly DateTime date;
+
+        public DateTime GetDate()
+        {
+            return date;
+        }
+        public double GetAmount()
+        {
+            return Amount;
+        }
+
+
+    }
+}
